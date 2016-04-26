3aa12c9d4746cfd73e9fe48b865009e2e02727eb
Eric NG <kinyanng@yahoo.com.hk>
2016-04-26 21:18:17 +0000

    update apartment models

--- models/apartment.js
+++ models/apartment.js
@@ -3,12 +3,13 @@ var mongoose = require('mongoose');
 
 var ApartmentSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    address: {type: String, required: true},
     company: {type: String, required: true},
     price: {type: Number, required: true},
     noOfBedroom: {type: Number, required: true},
     noOfBathRoom: {type: Number, required: true},
     image: {type: String},
+    lag: {type: Number, required: true},
+    lon: {type: Number, required: true},
     startLease: {type: Date, required: true},
     endLease: {type: Date, required: true}
 });
